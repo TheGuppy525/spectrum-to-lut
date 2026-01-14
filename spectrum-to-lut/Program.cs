dd60ea400d1628282cfd7e66f3a678fb14e3f55f
gup <guppu525@gmail.com>
2026-01-13 21:21:47 -0800

    first "functional"******* build

--- spectrum-to-lut/Program.cs
+++ spectrum-to-lut/Program.cs
@@ -1,6 +1,11 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.Numerics;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Tga;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.ColorSpaces;
+using SixLabors.ImageSharp.ColorSpaces.Conversion;
+
 
 namespace spectrum_to_lut
 {
@@ -8,8 +13,10 @@ namespace spectrum_to_lut
     {
         static void Main(string[] args)
         {
+            int WIDTH = 2048;
+            
             string inputPath = "input.png";
-            string outputPath = "output.cube";
+            string outputPath = "output.png";
             string spectrumRefPath = "spectrumRef.png";
             string cubeRefPath = "cubeRef.png";
             if (args.Length == 2)
@@ -28,8 +35,35 @@ namespace spectrum_to_lut
                 return;
             }
             
-            
+            for (int i = 0; i < WIDTH; i++)
+            {
+                Rgb refPixel = spectrumRef[i, 0]; 
+                Rgb spectrumPixel = spectrum[i, 0];
+                
+                var refHsv = ColorSpaceConverter.ToHsv(in refPixel);
+                float refHue = refHsv.H;
+                var spectrumHsv = ColorSpaceConverter.ToHsv(in spectrumPixel);
+                float spectrumHue = spectrumHsv.H;
 
+                float hueShift = (spectrumHue - refHue);
+
+                for (int h = 0; h < cubeRef.Height; h++)
+                {
+                    for (int w = 0; w < cubeRef.Width; w++)
+                    {
+                        Rgb cubePixel = cubeRef[w, h];
+                        var cubeHsv = ColorSpaceConverter.ToHsv(in cubePixel);
+                        float cubeHue = cubeHsv.H;
+
+                        if (Math.Abs(refHue - cubeHue) < 1)
+                        {
+                            cubeRef[w, h] = ColorSpaceConverter.ToRgb(new Hsv((cubeHue+hueShift), cubeHsv.V, cubeHsv.S));
+                        }
+                    }
+                }
+                Console.WriteLine(spectrumHue.ToString());
+            }
+            cubeRef.SaveAsPng(outputPath);
 
         }
     }
